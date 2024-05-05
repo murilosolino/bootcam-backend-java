7e2899fcbe5d39428da6dbc45eb91c4003c5ae56
Murilo Solino <murilo.solino@outlook.com>
2024-05-05 19:12:06 -0300

    desafio de projeto java basico

---
+++ projeto_conta_banco/ContaTerminal.java
@@ -0,0 +1,34 @@
+package projeto_conta_banco;
+
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+
+        int numeroConta;
+        String agencia;
+        String nomeCliente;
+        double saldo;
+
+        System.out.printf("Digite o número da conta: ");
+        numeroConta = scanner.nextInt();
+
+        System.out.printf("Digite a agência: ");
+        agencia = scanner.next();
+
+        scanner.nextLine();
+
+        System.out.printf("Digite o nome do cliente: ");
+        nomeCliente = scanner.nextLine();
+
+        System.out.printf("Digite o saldo: ");
+        saldo = scanner.nextDouble();
+
+        System.out.println("Olá, " + nomeCliente + "!" + "Obrigado por ter criado uma conta em nosso banco, sua agência é " + agencia + 
+        " e o número da sua conta é " + numeroConta + ". Seu saldo atual é de R$" + saldo + ".");
+
+        scanner.close();
+    }
+}
